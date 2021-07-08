e30bf7fd0891d40f36a1c799868feab25d2e5d66
thomaspaulwhalan <78250325+thomaspaulwhalan@users.noreply.github.com>
2021-07-08 18:10:06 +1000

    Create range.h

---
+++ range.h
@@ -0,0 +1,21 @@
+/*
+ *
+ * Programmed by Thomas Paul Whalan
+ *
+ * This program has been written to help search for strings within a file */
+
+#ifndef RANGE_H
+#define RANGE_H
+
+#ifndef STDLIB_H
+#include<stdlib.h>
+#endif
+#ifndef STDLIB_H
+#include<string.h>
+#endif
+
+int getleftvalue(char *args);
+
+int getrightvalue(char *args);
+
+#endif // RANGE_H
