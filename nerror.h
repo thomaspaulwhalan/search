f3f20e25a39dd8ae369572f641d20750e3f6e790
thomaspaulwhalan <78250325+thomaspaulwhalan@users.noreply.github.com>
2021-08-09 12:32:15 +1000

    Update nerror.h

--- nerror.h
+++ nerror.h
@@ -16,7 +16,7 @@
 /* Gives a return value (can be int, float, NULL, or left black */
 #ifndef 	FAIL_IF_R_M
 #define 	FAIL_IF_R_M(EXP, RETURN_VALUE, STREAM, MSG)	if(EXP){fprintf(STREAM, MSG);return RETURN_VALUE;}
-#endif	// NFAIL_IF_R_M
+#endif	// NFAIL_IF_R
 #endif	// NFAIL_IF_E_M
 
 /* Without fprintf() */
@@ -29,5 +29,14 @@
 #define 	FAIL_IF_R(EXP, RETURN_VALUE)			if(EXP){return RETURN_VALUE;}
 #endif	// NFAIL_IF_R
 #endif	// NFAIL_IF_E
+
+#ifndef		BREAK_IF
+#define		BREAK_IF(EXP)					if (EXP){break;}
+
+#ifndef		BREAK_IF_M
+#define		BREAK_IF_M(EXP, STREAM, MSG)			if (EXP){fprintf(STREAM, MSG);break;}
+
+#endif // BREAK_IF_R
+#endif // BREAK_IF
 
 #endif	// NERROR_H
